00c98250130a489d339088b77dfb4c4b7d61bf6d
Zoody <reopard506@gmail.com>
2025-01-31 17:12:11 +0900

    [🐛FIX]       : dashboard -> workflow 로 전환

---
+++ src/app/(workflow)/layout.tsx
@@ -0,0 +1,5 @@
+const WorkflowLayout = ({ children }: { children: React.ReactNode }) => {
+  return <main className="w-screen h-screen">{children}</main>;
+};
+
+export default WorkflowLayout;
---
+++ src/app/(workflow)/workflow/page.tsx
@@ -0,0 +1,9 @@
+import WorkFlow from "../_components/WorkFlow";
+
+const WorkflowPage = () => {
+  // TODO workflow 데이터를 서버사이드에서 프리패치 -> FlowEditor 에 전달
+
+  return <div className="relative w-full h-full">Workflow</div>;
+};
+
+export default WorkflowPage;
