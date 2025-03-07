3c44034afcf0e7661c814187ee581652ca10fce2
Zoody <reopard506@gmail.com>
2025-03-08 01:42:02 +0900

    [🎨STYLE]     : apps/web 에서 libs/core 컴포넌트의 테일윈드 구문을 해석하도록 content 에 libs/core 를 포함시킴

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -6,7 +6,7 @@ const baseConfig = require("../../tailwind.base.config");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "../libs/core/src/**/*.{js,ts,jsx,tsx}",
+    "../../libs/core/src/**/*.{js,ts,jsx,tsx}",
     join(
       __dirname,
       "{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}"
